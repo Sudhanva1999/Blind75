f02c399712672ed79828fef8f586720ea9084896
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2024-05-16 11:40:53 -0400

    begning changes

---
+++ Blind75/Questions.swift
@@ -0,0 +1,12 @@
+//
+//  Questions.swift
+//  Blind75
+//
+//  Created by Sudhanva Paturkar on 14/05/24.
+//
+
+import Foundation
+
+struct Questions {
+    
+}
---
+++ Blind75/RandomIndexGenerator.swift
@@ -0,0 +1,8 @@
+//
+//  RandomIndexGenerator.swift
+//  Blind75
+//
+//  Created by Sudhanva Paturkar on 16/05/24.
+//
+
+import Foundation
